198d9c48d47cac07e0e10a6b631bbe924a6cf43c
Kalantal <justinjrestivo@gmail.com>
2017-12-04 14:23:11 -0600

    cleanup

--- pumba_master.sh
+++ pumba_master.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #set -x
-exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 export container_name=$1
 export build_wait_time=60
@@ -45,37 +44,37 @@ while [ $# -gt 0 ]; do
 shift #this preserves the first argument $1 as the $container_name
 	case "$1" in
 		pumba_kill)
-				./tests/pumba_kill.sh &>>./results/log & ./tools/kill_script.sh $build_wait_time &
+				./tests/pumba_kill.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker kill test:\\n"
 				#Send termination signal to the main process inside target container(s)
 				;;
 		pumba_delay)
-				./tests/pumba_delay.sh & ./tools/kill_script.sh &
+				./tests/pumba_delay.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker delay test:\\n"			
 				#Delay egress traffic for specified containers; networks show variability so it is possible to add random variation; delay variation isn't purely random, so to emulate that there is a correlation
 				;;
 		pumba_pause)
-				./tests/pumba_pause.sh & ./tools/kill_script.sh &
+				./tests/pumba_pause.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker pause test:\\n"
 				#Stop the main process inside target containers, sending  SIGTERM, and then SIGKILL after a grace period
 				;;
 		pumba_stop)
-				./tests/pumba_stop.sh & ./tools/kill_script.sh &
+				./tests/pumba_stop.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker stop test:\\n"			
 				#Remove target containers, with links and volumes
 				;;
 		pumba_rm)
-				./tests/pumba_rm.sh & ./tools/kill_script.sh &
+				./tests/pumba_rm.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker rm test:\\n"			
 				#Pause all running processes within target containers
 				;;
 		pumba_netem_loss)
-				./tests/pumba_netem_loss.sh & ./tools/kill_script.sh &
+				./tests/pumba_netem_loss.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker netem_loss test:\\n"
 				#Adds packet losses, based on independent (Bernoulli) probability model
 				;;
 		pumba_netem_rate)
-				./tests/pumba_netem_rate.sh & ./tools/kill_script.sh &
+				./tests/pumba_netem_rate.sh &>./results/pumba_results.log & ./tools/kill_script.sh $build_wait_time &
 				echo -en "Docker netem_rate test:\\n"			
 				#Rate limit egress traffic for specified containers
 				;;
@@ -84,7 +83,7 @@ shift #this preserves the first argument $1 as the $container_name
 done
 
 #Wait for tests
-sleep $build_wait_time
+sleep $build_wait_time && echo -en "Done\\n"
 
 #Confirm clean enviornment
 #Using Docker instead of Pumba
@@ -97,11 +96,21 @@ if grep -q "test" /tmp/docker_ps;
 		echo -en "Containers stopped:\\n"
 		docker stop $(docker ps -a -q)
 	else
-		echo -en "No Containers to stop.\\n"
+		echo -en "No containers left over to stop\\n"
 fi
 }
 docker_clean
 
-echo -en "Done.\\n"
+docker_results() {
+sed -i '1d' ./results/pumba_results.log
+
+if grep -q "Killing" ./results/pumba_results.log; 
+	then
+		echo -en "Test injected sucessfully\\n"
+	else
+		echo -en "Test injection failed\\n"
+fi
+}
+docker_results
 
 exit 0
--- tools/kill_script.sh
+++ tools/kill_script.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+#pumba_linux_386 was not built to be automated. 
+#pumba_linux_386 does not stop itself from running when tasks are complete.
+#This initiates a forceful stop to the pumba related processes.
+
 export build_wait_time=$1
 
 sleep $build_wait_time
 
-sudo kill -9 `ps aux | grep pumba | awk '{print $2}'`
+for pid in $(ps -fe | grep '[p]umba' | grep -v grep | awk '{print $2}'); do
+    kill "$pid"
+done
+
+#Old. Catches the grep containing and throws an error because it can't find itself.
+#sudo kill -9 `ps aux | grep pumba | awk '{print $2}'`
 
 exit 0
