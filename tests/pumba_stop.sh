a55b76563c522f70256689814d27a1e81a30d84d
Justin Restivo <justinjrestivo@gmail.com>
2018-02-08 22:57:52 -0600

    Update pumba_stop.sh

--- tests/pumba_stop.sh
+++ tests/pumba_stop.sh
@@ -7,10 +7,10 @@ export pumba_results=./results/pumba_results.log
 
 #Stop the main process inside target containers, sending  SIGTERM, and then SIGKILL after a grace period
 
-echo -en "Pumba container stop test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container stop test:\\n" 2>&1 | tee -a $pumba_results
 
 pumba_stop_containers() {
-./pumba_linux_386 --interval 10s stop ubuntu-pumba $container
+./pumba_linux_386 --interval 10s stop $container
 }
 pumba_stop_containers 2>&1 | tee -a $pumba_results
 
