bbf231d323f4e31edc6d244054ab00932c2f5499
Justin Restivo <justinjrestivo@gmail.com>
2018-02-08 22:57:40 -0600

    Update pumba_rm.sh

--- tests/pumba_rm.sh
+++ tests/pumba_rm.sh
@@ -7,7 +7,7 @@ export pumba_results=./results/pumba_results.log
 
 #Remove target containers, with links and volumes
 
-echo -en "Pumba container rm test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container rm test:\\n" 2>&1 | tee -a $pumba_results
 
 pumba_rm_containers() {
 ./pumba_linux_386 --interval 10s rm $container
