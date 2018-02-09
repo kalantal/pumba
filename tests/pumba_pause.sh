85d733c1361acda7b142890827bba8b7e4420c16
Justin Restivo <justinjrestivo@gmail.com>
2018-02-08 22:57:34 -0600

    Update pumba_pause.sh

--- tests/pumba_pause.sh
+++ tests/pumba_pause.sh
@@ -7,7 +7,7 @@ export pumba_results=./results/pumba_results.log
 
 #Pause all running processes within target containers
 
-echo -en "Pumba container pause test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container pause test:\\n" 2>&1 | tee -a $pumba_results
 
 pumba_pause_containers() {
 ./pumba_linux_386 pause --duration 10s $container
