dd785fb5351d70e235f8d8a96e91ece44cd275d1
Justin Restivo <justinjrestivo@gmail.com>
2018-02-08 22:55:21 -0600

    Update pumba_all.sh

--- tests/pumba_all.sh
+++ tests/pumba_all.sh
@@ -7,31 +7,31 @@ export pumba_results=./results/pumba_results.log
 
 #Run all avaliable container tests
 
-echo -en "Pumba container kill test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container kill test:\\n" 2>&1 | tee -a $watcher_log
 timeout $test_wait_time ./pumba_linux_386 kill "$container"1 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
-echo -en "Pumba container delay test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container delay test:\\n" 2>&1 | tee -a $watcher_log
 timeout $test_wait_time ./pumba_linux_386 netem --duration 20s --tc-image gaiadocker/iproute2 delay --time 3000 jitter 50 --distribution normal "$container"2 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
-echo -en "Pumba container pause test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container pause test:\\n" 2>&1 | tee -a $watcher_log
 timeout $test_wait_time ./pumba_linux_386 pause --duration 10s "$container"3 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
-echo -en "Pumba container stop test:\\n" 2>&1 | tee -a ./results/pumba_results.log
-timeout $test_wait_time ./pumba_linux_386 --interval 10s stop ubuntu-pumba "$container"4 2>&1 | tee -a $pumba_results
+echo -en "Pumba container stop test:\\n" 2>&1 | tee -a $watcher_log
+timeout $test_wait_time ./pumba_linux_386 --interval 10s stop "$container"4 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
-echo -en "Pumba container rm test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container rm test:\\n" 2>&1 | tee -a $watcher_log
 timeout $test_wait_time ./pumba_linux_386 --interval 10s rm "$container"5 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
-echo -en "Pumba container netem loss test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container netem loss test:\\n" 2>&1 | tee -a $watcher_log
 timeout $test_wait_time ./pumba_linux_386 netem --duration 1m --tc-image gaiadocker/iproute2 loss --percent 20 "$container"6 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
-echo -en "Pumba container netem rate test:\\n" 2>&1 | tee -a ./results/pumba_results.log
+echo -en "Pumba container netem rate test:\\n" 2>&1 | tee -a $watcher_log
 timeout $test_wait_time ./pumba_linux_386 netem --duration 1m --tc-image gaiadocker/iproute2 rate "$container"7 2>&1 | tee -a $pumba_results
 sleep "$test_wait_time" && pkill pumba_linux_386
 
